2b22d49820b7857349f4947dc514c449504dd6ff
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-13 20:53:30 -0500

    Implementación de ejercicio básico con node js y npm express:
    - Se utilizaron los paquetes correspondientes para comunicar index.html, node index.js mediante el uso de express

---
+++ Lab_05/src/exerciseB.py
@@ -0,0 +1,15 @@
+from chessPictures import *
+from interpreter import draw
+
+# Caballos base (blanco y negativo/negro)
+caballo_blanco = knight
+caballo_negro = knight.negative()
+
+# Fila superior: blanco + negro
+fila_superior = caballo_blanco.join(caballo_negro)
+# Fila inferior: negro + blanco
+fila_inferior = caballo_negro.verticalMirror().join(caballo_blanco.verticalMirror())
+# Combinar ambas filas para mostrar 4 caballos
+tablero_final = fila_superior.up(fila_inferior)
+# Mostrar resultado
+draw(tablero_final)
