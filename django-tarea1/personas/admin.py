89d8ef56abba5fc71aed2b26ee15b7252266b336
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-20 15:34:59 -0500

    Configuración del panel admin para el modelo Contacto

--- django-tarea1/personas/admin.py
+++ django-tarea1/personas/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Contacto
+
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'telefono', 'fecha_registro', 'activo')
+    search_fields = ('nombre', 'email')
+    list_filter = ('activo', 'fecha_registro')
