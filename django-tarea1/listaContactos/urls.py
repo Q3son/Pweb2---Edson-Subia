00b106bc4956e76ff2d4e8e9453992865879fed7
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-20 15:49:13 -0500

    Configuración inicial de URLs

--- django-tarea1/listaContactos/urls.py
+++ django-tarea1/listaContactos/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  # Panel de administración
+    # path('', include('personas.urls')),  # Descomenta cuando crees urls.py en tu app
 ]
