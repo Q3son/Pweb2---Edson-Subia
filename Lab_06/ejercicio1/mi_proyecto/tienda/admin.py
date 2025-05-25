a2a31a5a010d8add8ab7092410c5961b29377167
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-25 18:58:37 -0500

    Configuramos el panel de administración, para:
    - El Registro de modelos Producto y Venta en admin
    - La personalización de listados con filtros y búsqueda
    - Y las mejoras en la visualización de datos

--- Lab_06/ejercicio1/mi_proyecto/tienda/admin.py
+++ Lab_06/ejercicio1/mi_proyecto/tienda/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Producto, Venta
 
-# Register your models here.
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'precio', 'stock')
+    search_fields = ('codigo', 'nombre')
+    list_filter = ('precio',)
+
+@admin.register(Venta)
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('producto', 'cantidad', 'fecha', 'cliente')
+    list_filter = ('fecha', 'producto')
+    date_hierarchy = 'fecha'
