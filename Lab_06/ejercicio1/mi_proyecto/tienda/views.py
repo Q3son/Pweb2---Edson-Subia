0c6a5ec6d0d6b0d0ad9627095b52bd843a270bea
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-25 19:05:35 -0500

    Implementamos las vistas (views.py) del proyecto
    - 1 Vista basada en clase para listado de productos
    -1 Vista basada en clase para creación de ventas
    - 2 Vistas funcionales para listado de ventas y detalle de producto
    - Optimización de consultas con select_related

--- Lab_06/ejercicio1/mi_proyecto/tienda/views.py
+++ Lab_06/ejercicio1/mi_proyecto/tienda/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView
+from .models import Producto, Venta
+from .forms import VentaForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'tienda/home.html')
+
+class ListaProductosView(ListView):
+    model = Producto
+    template_name = 'tienda/lista_productos.html'
+    context_object_name = 'productos'
+    paginate_by = 10
+
+class CrearVentaView(CreateView):
+    model = Venta
+    form_class = VentaForm
+    template_name = 'tienda/crear_venta.html'
+    success_url = '/ventas/'
+
+def lista_ventas(request):
+    ventas = Venta.objects.select_related('producto').order_by('-fecha')
+    return render(request, 'tienda/lista_ventas.html', {'ventas': ventas})
+
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    ventas = Venta.objects.filter(producto=producto).order_by('-fecha')
+    return render(request, 'tienda/detalle_producto.html', {
+        'producto': producto,
+        'ventas': ventas
+    })
