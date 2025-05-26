8c091fda49dec78d2513e3b264dca3db7f7d4c2e
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-25 21:49:59 -0500

    Configuración del administrador
    - Se crearon interfaces personalizadas para cada modelo
    - Se usaron filtros y búsquedas optimizadas
    - Se logró la visualización de campos relacionados
    - Y se organizó con jerarquía de fechas para las notas respectivas

--- Lab_06/ejercicio2/sistema_notas/educacion/admin.py
+++ Lab_06/ejercicio2/sistema_notas/educacion/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Alumno, Curso, NotaAlumnoPorCurso
 
-# Register your models here.
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('dni', 'apellido', 'nombre', 'email')
+    search_fields = ('dni', 'apellido', 'nombre')
+    list_filter = ('apellido',)
+    ordering = ('apellido', 'nombre')
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'creditos', 'profesor')
+    search_fields = ('codigo', 'nombre', 'profesor')
+    list_filter = ('creditos',)
+
+@admin.register(NotaAlumnoPorCurso)
+class NotaAlumnoPorCursoAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'curso', 'nota', 'fecha_evaluacion')
+    search_fields = ('alumno__apellido', 'alumno__nombre', 'curso__nombre')
+    list_filter = ('curso', 'fecha_evaluacion')
+    raw_id_fields = ('alumno', 'curso')
+    date_hierarchy = 'fecha_evaluacion'
