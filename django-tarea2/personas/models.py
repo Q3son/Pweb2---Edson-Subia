c63449b84efcc7ef719298b3417b99f9d0055b7c
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-30 17:29:43 -0500

    Cambiando el modelo y Actualizando los campos Antiguos ----Diapo 5 y 6---- :
    Se agregó el campo booleano donador al modelo Persona, marcando si el contacto es donador o no.
    
    Durante la migración, se asignó automáticamente el valor True a los registros existentes (Mario y Luigi) para evitar conflictos con la base de datos.
    
    ¿Cómo crear un campo obligatorio?
    
    Un campo obligatorio requiere dos configuraciones:
    - blank=False: Obliga a completar el campo en formularios (aparece en negrita).
    
    - null=False: Evita valores nulos en la base de datos.
    Ejemplo: Un campo "nombre" sin valores vacíos ni nulos.
    
    ¿Qué parámetros afectan a la base de datos?
    
    Afectan directamente:
    - null (permite/prohíbe NULL en la BD).
    - default (valor por defecto al crear registros).
    - unique (restricción de unicidad en la BD).
    - No afectan la BD (solo lógica de la aplicación):
    - blank (validación en formularios).
    - verbose_name (etiqueta descriptiva).
    - choices (opciones predefinidas).

--- django-tarea2/personas/models.py
+++ django-tarea2/personas/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
-    edad = models.IntegerField()
+    edad = models.IntegerField()#Max digits 3
+    donador = models.BooleanField()
     
     class Meta:
         verbose_name = "Persona"
