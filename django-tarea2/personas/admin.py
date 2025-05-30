ebe8a090fd1453cd13fa61f3ecd2e6a32e397aab
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-30 17:01:44 -0500

    Cambiando los tipos de campos - Diapo 3:
    - Logramos cambiar la clase Contacto por la Clase persona con tipos de datos más apropiados para sus campos, asismismo, configuramos correctamente admin.py, para que haga referencia a la nueva clase Persona y no a la antigua clase Contactos

--- django-tarea2/personas/admin.py
+++ django-tarea2/personas/admin.py
@@ -1,17 +1,8 @@
+# personas/admin.py
 from django.contrib import admin
-from django.utils import timezone  # ¡Importación faltante!
-from .models import Contacto
+from .models import Persona  # Cambiar Contacto por Persona
 
-@admin.register(Contacto)
-class ContactoAdmin(admin.ModelAdmin):
-    def formatted_fecha(self, obj):
-        return timezone.localtime(obj.fecha_registro).strftime('%d/%m/%Y %H:%M')
-    formatted_fecha.short_description = 'Fecha registro'
-    formatted_fecha.admin_order_field = 'fecha_registro'
-    
-    # Usa EL MISMO nombre en list_display (o usa el método personalizado)
-    ordering = ('-fecha_registro',)
-    list_display = ('nombre', 'email', 'telefono', 'fecha_registro', 'formatted_fecha')
-    search_fields = ('nombre', 'email')
-    list_filter = ('fecha_registro', 'activo')
-    date_hierarchy = 'fecha_registro'
+@admin.register(Persona)  # Actualizar aquí también
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellidos', 'edad')
+    search_fields = ('nombre', 'apellidos')
