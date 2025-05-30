147fbdf0b05590afb7311ffb8d7453ed07854304
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-05-30 18:06:41 -0500

    Plantillas templates de Django y Django template language (DTL): Variables  --- Diapo 12 y 13 :
    Estructura de templates mejorada:
    
    - Creación de templates/home.html con diseño personalizado.
    - Uso de variables DTL ({{ request.user }}, {{ request.user.is_authenticated }}).
    - Configuración en settings.py:
    - Verificado que DIRS apunte a BASE_DIR / "templates".
    - Actualización de la vista myHomeView:
    - Ahora usa render() para cargar la plantilla.

--- django-tarea2/inicio/views.py
+++ django-tarea2/inicio/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def myHomeView(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+def myHomeView(request):
+    """Vista principal que será migrada a template en el siguiente commit"""
+    print(request.user)  # Debug: ver usuario en consola
+    return render(request, "home.html")  # Preparada para template
 
 def anotherView(request):
-    return HttpResponse('<h1>Sólo otra página</h1>')
+    """Vista secundaria (se actualizará luego)"""
+ #   return HttpResponse('<h1>Sólo otra página</h1>')
