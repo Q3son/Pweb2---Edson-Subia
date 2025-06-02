614d33223b0607c89843f5403137e164f47bcfb9
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-02 16:46:35 -0500

    Modificamos todas las variables en models.py (clase DestinoTuristico) y las migramos a la DB de psql:
    - Reemplazo total de `Destination` por modelo en español
    - Campos renombrados para claridad (nombreCiudad, precioTour)
    - Configuración mejorada para el admin de Django
    - Ajustes en vistas y templates"

--- Lab_07 - django_destinos/travello/views.py
+++ Lab_07 - django_destinos/travello/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Destination
+from .models import DestinoTuristico
 # Create your views here.
 
 
 def index(request):
 
-    dests = Destination.objects.all()
+    dests = DestinoTuristico.objects.all()
 
     return render(request, "index.html", {'dests': dests})
