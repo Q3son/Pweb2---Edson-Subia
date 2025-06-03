ba53c3631eea43ec57d34884426dd037c4a4bdc5
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-02 22:22:50 -0500

    Terminamos de configurar nuestra BD PostgreSQL, para reiniciar nuestro proyecto de destinos turísticos, asimismo, configuramos el idioma y zona horaria de nuestro servidor

--- Lab_07 - django_destinos/perutourism/perutourism/settings.py
+++ Lab_07 - django_destinos/perutourism/perutourism/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'perutourism.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'perutourism_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -102,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
