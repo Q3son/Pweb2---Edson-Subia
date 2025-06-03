7d1e80a5865f0a84cd8b33b5162bba80ab6c40c5
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-02 22:25:18 -0500

    Configuramos los modelos y variables a usar en el proyecto y los migramos a la BD

--- Lab_07 - django_destinos/perutourism/destinos/models.py
+++ Lab_07 - django_destinos/perutourism/destinos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    descripcionCiudad = models.TextField()
+    imagenCiudad = models.ImageField(upload_to='destinos/')
+    precioTour = models.DecimalField(max_digits=8, decimal_places=2)
+    ofertaTour = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombreCiudad
