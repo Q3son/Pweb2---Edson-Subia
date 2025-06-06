31cc9a9e95f2977787d67f4c4401555859e6d147
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-05 23:52:40 -0500

    Examinamos los Objetos del Modelo ---> Diapo 9: Los objetos del modelo contienen los campos que definimos y algunos otros más heredados del framework

--- django-tarea3/personas/models.py
+++ django-tarea3/personas/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
-    edad = models.IntegerField()#Max digits 3
+    edad = models.IntegerField(blank = True)#Max digits 3
     donador = models.BooleanField()
     
     class Meta:
