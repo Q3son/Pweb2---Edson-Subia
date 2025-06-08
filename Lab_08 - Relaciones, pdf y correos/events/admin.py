886edc289644979d873cb3d2b9874b24e2622850
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-08 16:12:44 -0500

    Ahora, registramos los Nuevos Modelos en el Admin (admin.py), para el correcto funcioanmiento del proyecto

--- Lab_08 - Relaciones, pdf y correos/events/admin.py
+++ Lab_08 - Relaciones, pdf y correos/events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# En esta parte, añadiremos los nuevos modelos a la importación
+from .models import Venue, Event, Language, Framework, Simple, DateExample, NullExample
+
+# Y registramos los modelos originales (cree dos extra para mis propios experimentos y personalización)
+admin.site.register(Venue)
+admin.site.register(Event)
+
+# También registramos los modelos del vídeo
+admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Simple)
+admin.site.register(DateExample)
+admin.site.register(NullExample)
