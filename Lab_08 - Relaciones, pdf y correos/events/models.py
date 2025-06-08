4d52e3841253e93e1c1c1b84739dc7a258cfd7c7
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-08 16:06:20 -0500

    Modificamos y añadimos los modelos correspondientes y a usar en model.py

--- Lab_08 - Relaciones, pdf y correos/events/models.py
+++ Lab_08 - Relaciones, pdf y correos/events/models.py
@@ -1,2 +1,46 @@
 from django.db import models
+#BY: SUBIA EDSON
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=120)
+    address = models.CharField(max_length=300)
+    # ... (resto de los campos de Venue)
+    def __str__(self):
+        return self.name
 
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    # ... (resto de los campos de Event)
+    def __str__(self):
+        return self.name
+
+# --- Añadiré los modelos del vídeo como réplica ---
+
+class Language(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Framework(models.Model):
+    name = models.CharField(max_length=10)
+    # Esta es la relación uno-a-muchos que muestra el video
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Simple(models.Model):
+    text = models.CharField(max_length=10)
+    number = models.IntegerField(null=True)
+    url = models.URLField(default='www.example.com')
+
+    def __str__(self):
+        return self.url
+
+class DateExample(models.Model):
+    the_date = models.DateTimeField()
+
+class NullExample(models.Model):
+    col = models.CharField(max_length=10, blank=True, null=True)
