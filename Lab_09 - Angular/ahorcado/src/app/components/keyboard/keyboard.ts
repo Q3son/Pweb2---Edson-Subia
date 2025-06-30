a7132f146bdc410dd0523d2e43d599754a1ced48
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-30 00:06:54 -0500

    Configuramos la lógica del componente para el teclado (keyboard), se aceptan todas las letras a excepción de la Ñ por comodidad general.

--- Lab_09 - Angular/ahorcado/src/app/components/keyboard/keyboard.ts
+++ Lab_09 - Angular/ahorcado/src/app/components/keyboard/keyboard.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-keyboard',
-  imports: [],
+  standalone: true,
+  imports: [MatButtonModule],
   templateUrl: './keyboard.html',
   styleUrl: './keyboard.css'
 })
 export class Keyboard {
+  // Lista de letras ya intentadas para deshabilitar los botones (lo recibe)
+  public triedLetters = input.required<string[]>();
+  
+  // Avisa al componente padre (Game -> mi juego) que una letra fue presionada
+  @Output() letterPressed = new EventEmitter<string>();
 
+  public alphabet: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+  onLetterClick(letter: string) {
+    this.letterPressed.emit(letter);
+  }
 }
