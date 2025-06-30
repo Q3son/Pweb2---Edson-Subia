2f7de90aab116626f142b56403609e4590f0601f
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-29 23:41:12 -0500

    Implementamos la lógica, presentación y estilo de el componente figure

--- Lab_09 - Angular/ahorcado/src/app/components/figure/figure.ts
+++ Lab_09 - Angular/ahorcado/src/app/components/figure/figure.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-figure',
+  standalone: true,
   imports: [],
   templateUrl: './figure.html',
   styleUrl: './figure.css'
 })
 export class Figure {
+  
+  public mistakesCount = input.required<number>();
 
 }
