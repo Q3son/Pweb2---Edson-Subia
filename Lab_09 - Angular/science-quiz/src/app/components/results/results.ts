6c3e894292dc10fbd828cdaba0100b25fd277383
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-29 23:00:29 -0500

    Implementamos al componente results para el quizz

--- Lab_09 - Angular/science-quiz/src/app/components/results/results.ts
+++ Lab_09 - Angular/science-quiz/src/app/components/results/results.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { QuizService } from '../../services/quiz.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common'; // Necesario para ngClass
 
 @Component({
   selector: 'app-results',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, MatCardModule, MatButtonModule, MatListModule, MatIconModule],
   templateUrl: './results.html',
   styleUrl: './results.css'
 })
 export class Results {
+  quizService = inject(QuizService);
+  router = inject(Router);
 
+  restartQuiz() {
+    this.router.navigate(['/']);
+  }
 }
