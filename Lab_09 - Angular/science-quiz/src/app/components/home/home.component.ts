193ac680ab4f7fa4521f7b387690e0c4fc906281
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-29 22:55:27 -0500

    Configuramos la lógica en el componente home, para el funcionamiento del Quiz

--- Lab_09 - Angular/science-quiz/src/app/components/home/home.component.ts
+++ Lab_09 - Angular/science-quiz/src/app/components/home/home.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { QuizService } from '../../services/quiz.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, MatButtonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  quizService = inject(QuizService);
+  router = inject(Router);
 
+  startQuiz() {
+    this.quizService.reset();
+    this.router.navigate(['/question']);
+  }
 }
