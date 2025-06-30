169b410347879c97e38d365f4b9651beb4226bfe
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-06-30 00:11:54 -0500

    Unimos todos los componentes del juego en (Game.ts), para que el juego corra correctamente, a su vez, inyectamos el servicio de la app para que tengamos acceso a todos los componentes usados

--- Lab_09 - Angular/ahorcado/src/app/components/game/game.ts
+++ Lab_09 - Angular/ahorcado/src/app/components/game/game.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Hangman } from '../../services/hangman';
+import { Figure } from '../figure/figure';
+import { Keyboard } from '../keyboard/keyboard';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  standalone: true,
+  imports: [
+    Figure,
+    Keyboard,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './game.html',
   styleUrl: './game.css'
 })
 export class Game {
-
+  // Inyectamos el servicio de la app para que tengamos acceso a toda la lógica y estado de los componentes totales.
+  public hangman = inject(Hangman);
 }
