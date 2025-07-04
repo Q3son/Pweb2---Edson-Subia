f28d0798dff36549cd505f6e4c38e62733e4ba5a
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-03 20:04:36 -0500

    Slide 1: "Verificamos y creamos los archivos correspondientes a la tarea actual"

--- angular-tarea/my-dream-app/src/app/app.ts
+++ angular-tarea/my-dream-app/src/app/app.ts
@@ -8,5 +8,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'my-dream-app';
+  title = 'my-dream-app';
+  name = "Edson Fabricio Subia Huaicane";
+  email = "esubiahu@unsa.edu.pe";
+  webpage = "https://www.unsa.edu.pe";
 }
