077a71138fd36ac7f0b7bde992a5889a7202765a
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-03 20:53:15 -0500

    Slide 6: Generamos componente hello - world, lo configuramos en su propio TypeScript

---
+++ angular-tarea/my-dream-app/src/app/hello-world/hello-world.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  imports: [],
+  templateUrl: './hello-world.html',
+  styleUrl: './hello-world.css'
+})
+export class HelloWorld implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
+  
+}
