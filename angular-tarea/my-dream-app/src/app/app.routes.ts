8bc1b9081374f75885808d892d9893f404179325
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-04 00:14:48 -0500

    Slide 3: Insertando Ruteos /about y /hello al proyecto

--- angular-tarea/my-dream-app/src/app/app.routes.ts
+++ angular-tarea/my-dream-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { HelloWorld } from './hello-world/hello-world';
+import { App } from './app';
+export const routes: Routes = [
+    // La ruta para '/about' mostrará el AboutComponent
+    { path: 'about', component: About },
 
-export const routes: Routes = [];
+    // La ruta para '/hello' mostrará el HelloWorldComponent
+    { path: 'hello', component: HelloWorld },
+];
