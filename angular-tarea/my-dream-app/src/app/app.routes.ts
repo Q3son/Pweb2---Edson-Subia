6afec6e405fe90069a5b24a2c572a2ce2e6879a5
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-05 14:26:38 -0500

    Slide 6: Terminamos la configuración del home component y damos estilo final a los placeholder

--- angular-tarea/my-dream-app/src/app/app.routes.ts
+++ angular-tarea/my-dream-app/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
 import { HelloWorld } from './hello-world/hello-world';
+import { Home } from './home/home';
 export const routes: Routes = [
-    { path: '', redirectTo: '/about', pathMatch: 'full' },
+    { path: '', component: Home},
     // La ruta para '/about' mostrará el AboutComponent
     { path: 'about', component: About },
 
     // La ruta para '/hello' mostrará el HelloWorldComponent
     { path: 'hello', component: HelloWorld },
-    { path: '**', redirectTo: '/about' }
+    { path: '**', redirectTo: '' }
 ];
--- angular-tarea/my-dream-app/src/app/app.ts
+++ angular-tarea/my-dream-app/src/app/app.ts
@@ -1,81 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common'; 
-import { HelloWorld } from './hello-world/hello-world';
-import { User } from './user/user';
-import { FormsModule } from '@angular/forms';
-import { Data } from './data'; 
-import { Post } from './Post';  
-import { Router } from 'express';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule, 
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    HelloWorld,
-    User,
-    FormsModule,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
+export class App {
   title = 'my-dream-app';
-  name : string;
-  age : number;
-  email; // : string :
-  webpage: string;
-  hobbies: string[];
-  showHobbies: boolean;
-  posts: Post[] = [];
-  users = ['Edson','Jose','Jonathan','Valeria'];
-  activated = false;
-
-  constructor(private dataService: Data){
-    console.log("Constructor working...");
-    this.name = "Edson Fabricio Subia Huaicane";
-    this.age = 20;
-    this.email = "esubiahu@unsa.edu.pe";
-    this.webpage = "https://www.unsa.edu.pe";
-    this.hobbies = ["Basquet","Games Developing","HBO-MAX"];
-    this.showHobbies = false;
-  }
-  ngOnInit() {
-    this.dataService.getData().subscribe(data => {
-      //console.log(data);
-      this.posts = data;
-    });
-  }
-  toggleHobbies(){
-    this.showHobbies = !this.showHobbies;
-  }
-  newHobby(hobby: { value: string; }){
-    //console.log(hobby.value);
-    this.hobbies.push(hobby.value);
-    hobby.value = "";
-    return false;
-  }
-  
-  sayHello(){
-    alert("Hola desde App");
-  }
-  addUser(newUser: { value: string; focus: () => void; }){
-    //console.log(newUser.value);
-    this.users.push(newUser.value);
-    newUser.value = '';
-    newUser.focus();
-    return false;
-  }
-  
-  deleteUser(user: string){
-    for(let i=0; i<this.users.length; i++){
-      if (user==this.users[i]){
-        this.users.splice(i,1);
-      }
-    }
-  }
-
 }
