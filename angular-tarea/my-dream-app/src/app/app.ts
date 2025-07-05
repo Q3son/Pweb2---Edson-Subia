b9058e26a2653f067c3a6dcdc1e0dfaf753a365e
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-05 17:22:49 -0500

    Slide 8: (diapos 28, 29 y 30): Integramos Bootstrap a nuestra webpage

--- angular-tarea/my-dream-app/src/app/app.ts
+++ angular-tarea/my-dream-app/src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CollapseModule } from 'ngx-bootstrap/collapse'; // Para el menú hamburguesa
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'; // Para menús desplegables
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,13 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
+    CollapseModule,
+    BsDropdownModule
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'my-dream-app';
+  isCollapsed = true;
 }
