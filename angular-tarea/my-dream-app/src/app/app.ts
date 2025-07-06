9bf6d6a238fab690091a4c907f9b4f0ddc584b01
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-05 19:00:39 -0500

    Slide 9: Implementamos el proyecto + BOOTSTRAP

--- angular-tarea/my-dream-app/src/app/app.ts
+++ angular-tarea/my-dream-app/src/app/app.ts
@@ -1,22 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { CollapseModule } from 'ngx-bootstrap/collapse'; // Para el menú hamburguesa
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown'; // Para menús desplegables
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive,
-    CollapseModule,
-    BsDropdownModule
+    RouterLinkActive
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'my-dream-app';
-  isCollapsed = true;
 }
