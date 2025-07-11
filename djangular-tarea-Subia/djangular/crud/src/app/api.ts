b425fcca53b94d4d46f2ec49c2bf908aa8e72615
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-10 22:07:19 -0500

    Slide 9: Configuramos app.component.ts: - Creamos el arreglo de movies, el constructor con su getter, usamos 'api'. - Configuramos la obtención de errores.

--- djangular-tarea-Subia/djangular/crud/src/app/api.ts
+++ djangular-tarea-Subia/djangular/crud/src/app/api.ts
@@ -4,5 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class Api {
+  getAllMovies() {
+    throw new Error('Method not implemented.');
+  }
   
 }
--- djangular-tarea-Subia/djangular/crud/src/app/app.ts
+++ djangular-tarea-Subia/djangular/crud/src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Api } from './api';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  providers: [Api]
 })
 export class App {
   protected readonly title = signal('crud');
+  movies = [{id:1,title:'peli1',year:2021},{id:2,title:'peli2',year:2022}];
+  constructor(private api:Api) {
+    this.getMovies();
+  }
+  getMovies = () => {
+    this.api.getAllMovies().subscribe (
+      (data: any) => {
+        console.log('Datos recibidos:', data);
+        this.movies = data;  
+        data.results;
+      },
+      (error: any) => {
+        console.error('Error al obtener datos:', error);
+      } 
+    ) 
+  } 
 }
